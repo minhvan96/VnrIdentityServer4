638360c47d6b81923cfdbbfd1212f4cdded60bc3
minhvan96 <74522363+minhvan96@users.noreply.github.com>
2021-01-11 22:42:43 +0700

    add azure ad, update later

--- VnR.IdentityServer/Program.cs
+++ VnR.IdentityServer/Program.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using Azure.Identity;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Serilog;
 using Serilog.Events;
@@ -51,6 +54,29 @@ namespace StsServer
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                //.ConfigureAppConfiguration((context, config) =>
+                //{
+                //    if (context.HostingEnvironment.IsProduction())
+                //    {
+                //        var builtConfig = config.Build();
+
+                //        using (var store = new X509Store(StoreLocation.CurrentUser))
+                //        {
+                //            store.Open(OpenFlags.ReadOnly);
+                //            var certs = store.Certificates
+                //                .Find(X509FindType.FindByThumbprint,
+                //                    builtConfig["AzureADCertThumbprint"], false);
+
+                //            config.AddAzureKeyVault(new Uri($"https://{builtConfig["KeyVaultName"]}.vault.azure.net/"),
+                //                                    new ClientCertificateCredential(builtConfig["AzureADDirectoryId"], builtConfig["AzureADApplicationId"], certs.OfType<X509Certificate2>().Single()),
+                //                                    new KeyVaultSecretManager());
+
+                //            config.AddJsonFile("appsettings.json", optional: false)
+                //                    .AddJsonFile($"appsettings.{context.HostingEnvironment.EnvironmentName}.json", optional: true);
+                //            store.Close();
+                //        }
+                //    }
+                //})
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>()
